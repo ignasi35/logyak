764df7a1bf8cae512f68fe16b47e2d43289f495f
avilatusell <avilatusell@gmail.com>
2016-09-06 11:17:56 +0200

    modifies template list kayakers. Adds ranking and miles

--- server/models/schemas/kayaker.js
+++ server/models/schemas/kayaker.js
@@ -1,6 +1,8 @@
 var kayakerSchema = {
 	name: { type: String, required: true, trim: true },
 	surname: { type: String, required: true, trim: true },
+	ranking: { type: Number },
+	miles: { type: Number },
 	username: { type: String, required: true, trim: true },
 	kayakMaterial: { type: String },
 	notes: { type: String }
